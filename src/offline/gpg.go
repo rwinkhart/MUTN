207a4762b68f24a34b9e47fef5e82cb625724433
Randall Winkhart <idgr@tutanota.com>
2024-03-03 22:15:35 -0500

    Handle errors resulting from failed decryption

--- src/offline/gpg.go
+++ src/offline/gpg.go
@@ -1,6 +1,8 @@
 package offline
 
 import (
+	"fmt"
+	"os"
 	"os/exec"
 	"strings"
 )
@@ -11,7 +13,11 @@ import (
 // DecryptGPG decrypts a GPG-encrypted file and returns the contents as a slice of (trimmed) strings
 func DecryptGPG(targetLocation string) []string {
 	cmd := exec.Command("gpg", "--pinentry-mode", "loopback", "-q", "-d", targetLocation)
-	output, _ := cmd.CombinedOutput()
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println(AnsiError + "Failed to decrypt \"" + targetLocation + "\" - ensure it is a valid GPG-encrypted file and that you entered your passphrase correctly" + AnsiReset)
+		os.Exit(1)
+	}
 	outputSlice := strings.Split(string(output), "\n")
 	for i, lineData := range outputSlice {
 		outputSlice[i] = strings.TrimRight(lineData, " \t") // remove trailing whitespace
