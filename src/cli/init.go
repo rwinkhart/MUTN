8c1eaf3d65fdfbbef7c39c74f9a27cd9487bd785
Randall Winkhart <idgr@tutanota.com>
2024-05-17 12:25:10 -0400

    Fix import cycle (over SSH keyfile passphrase input)

--- src/cli/init.go
+++ src/cli/init.go
@@ -32,7 +32,7 @@ func TempInitCli() {
 	configSSH := inputBinary("Configure SSH settings (for synchronization)?")
 	if configSSH {
 		// necessary SSH info
-		fmt.Print(AnsiBold+"Note:"+backend.AnsiReset, "Only key-based authentication is supported (keys may optionally be passphrase-protected).\nThe remote server must already be in your ~/.ssh/known_hosts file.\n\n")
+		fmt.Println(AnsiBold + "\nNote:" + backend.AnsiReset + " Only key-based authentication is supported (keys may optionally be passphrase-protected).\nThe remote server must already be in your ~/.ssh/known_hosts file.")
 		sshUser := input("Remote SSH username:")
 		sshIP := input("Remote SSH IP address:")
 		sshPort := input("Remote SSH port:")
@@ -40,7 +40,7 @@ func TempInitCli() {
 		sshKeyProtected := inputBinary("Is the identity file password-protected?")
 
 		// write config file
-		backend.TempInit(map[string]string{"textEditor": textEditor, "gpgID": gpgID, "sshUser": sshUser, "sshIP": sshIP, "sshPort": sshPort, "sshIdentity": sshKey, "sshIDProtected": strconv.FormatBool(sshKeyProtected)})
+		backend.TempInit(map[string]string{"textEditor": textEditor, "gpgID": gpgID, "sshUser": sshUser, "sshIP": sshIP, "sshPort": sshPort, "sshKey": sshKey, "sshKeyProtected": strconv.FormatBool(sshKeyProtected)})
 
 		// generate device ID
 		sync.DeviceIDGen()
