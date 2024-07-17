4daa031c0e25d26b4661bd1a80426b3b67148ff7
Randall Winkhart <idgr@tutanota.com>
2024-07-17 15:49:45 -0400

    Allow writing to different section headers in libmutton.ini

--- src/cli/initUNIX.go
+++ src/cli/initUNIX.go
@@ -10,7 +10,6 @@ const fallbackEditor = "vi" // vi is pre-installed on most UNIX systems
 
 // textEditorFallback returns the value of the $EDITOR environment variable, or FallbackEditor if it is not set
 func textEditorFallback() string {
-	// ensure textEditor is set
 	textEditor := os.Getenv("EDITOR")
 	if textEditor == "" {
 		textEditor = fallbackEditor
